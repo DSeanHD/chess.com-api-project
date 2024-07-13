5d655987f1a89f3fdf430ffad9ab588642aa488d
DSeanHD <danielseanfolkes@gmail.com>
2024-07-13 16:57:16 -0500

    Added error handling to search area

--- script.js
+++ script.js
@@ -24,73 +24,80 @@ const chessCall = async () => {
         return;
     }
 
-    const res = await fetch(`https://api.chess.com/pub/player/${userInput.value}`);
-    const data = await res.json();
-    console.log(data);
+    try {
+        const res = await fetch(`https://api.chess.com/pub/player/${userInput.value}`);
+        const data = await res.json();
+        console.log(data);
 
-    const resStats = await fetch(`https://api.chess.com/pub/player/${userInput.value}/stats`);
-    const dataStats = await resStats.json();
-    console.log(dataStats);
+        const resStats = await fetch(`https://api.chess.com/pub/player/${userInput.value}/stats`);
+        const dataStats = await resStats.json();
+        console.log(dataStats);
 
-    const resCountry = await fetch(`${data.country}`);
-    const dataCountry = await resCountry.json();
-    console.log(dataCountry);
+        const resCountry = await fetch(`${data.country}`);
+        const dataCountry = await resCountry.json();
+        console.log(dataCountry);
 
-    let blitzHtml = '';
-    let bulletHtml = '';
-    let titleHtml = '';
+        let blitzHtml = '';
+        let bulletHtml = '';
+        let titleHtml = '';
 
-    if (dataStats.chess_blitz) {
-        blitzHtml = `
-            <div class="blitz">
-                <p>Blitz</p>
-                <p>Rating: ${dataStats.chess_blitz.last.rating}</p>
-                <p>Win/Loss/Draw Record: ${dataStats.chess_blitz.record.win}/${dataStats.chess_blitz.record.loss}/${dataStats.chess_blitz.record.draw}</p>
+        if (dataStats.chess_blitz) {
+            blitzHtml = `
+                <div class="blitz">
+                    <p>Blitz</p>
+                    <p>Rating: ${dataStats.chess_blitz.last.rating}</p>
+                    <p>Win/Loss/Draw Record: ${dataStats.chess_blitz.record.win}/${dataStats.chess_blitz.record.loss}/${dataStats.chess_blitz.record.draw}</p>
+                </div>
+                <br>
+            `;
+        }
+
+        if (dataStats.chess_bullet) {
+            bulletHtml = `
+                <div class="bullet">
+                    <p>Bullet</p>
+                    <p>Rating: ${dataStats.chess_bullet.last.rating}</p>
+                    <p>Win/Loss/Draw Record: ${dataStats.chess_bullet.record.win}/${dataStats.chess_bullet.record.loss}/${dataStats.chess_bullet.record.draw}</p>
+                </div>
+                <br>
+            `;
+        }
+
+        if (data.title) {
+            titleHtml = `<span class="title">${data.title}</span>`;
+        }
+
+        if (data.followers >= 1000 && data.followers <= 999999) {
+            data.followers = `${(data.followers / 1000).toFixed(2)}k`;
+        } else if (data.followers >= 1000000) {
+            data.followers = `${(data.followers / 1000000).toFixed(2)}m`;
+        }
+        
+
+        displayData.innerHTML = `
+            <img src="${data.avatar}" alt="${userInput.value}'s Avatar">
+            <h2>Name: ${titleHtml} ${data.name}</h2>
+            <h3>Username: ${data.username}</h3>
+            <p><b>Followers:</b> ${data.followers}</p>
+            <p><b>Country:</b> ${dataCountry.name}</p>
+            <p><b>League:</b> ${data.league}</p>
+            <p><b>Chess.com URL:</b> <a target="_blank" href="${data.url}">${data.url}</a></p>
+            <br>
+            <div class="rapid">
+                <p>Rapid</p>
+                <p>Rating: ${dataStats.chess_rapid.last.rating}</p>
+                <p>Win/Loss/Draw Record: ${dataStats.chess_rapid.record.win}/${dataStats.chess_rapid.record.loss}/${dataStats.chess_rapid.record.draw}</p>
             </div>
             <br>
+            ${blitzHtml}
+            ${bulletHtml}
         `;
     }
-
-    if (dataStats.chess_bullet) {
-        bulletHtml = `
-            <div class="bullet">
-                <p>Bullet</p>
-                <p>Rating: ${dataStats.chess_bullet.last.rating}</p>
-                <p>Win/Loss/Draw Record: ${dataStats.chess_bullet.record.win}/${dataStats.chess_bullet.record.loss}/${dataStats.chess_bullet.record.draw}</p>
-            </div>
-            <br>
-        `;
+    catch (err) {
+        console.log(err.message);
+        displayData.innerHTML = `<h3>This player is unavailable</h3>`;
     }
 
-    if (data.title) {
-        titleHtml = `<span class="title">${data.title}</span>`;
-    }
-
-    if (data.followers >= 1000 && data.followers <= 999999) {
-        data.followers = `${(data.followers / 1000).toFixed(2)}k`;
-    } else if (data.followers >= 1000000) {
-        data.followers = `${(data.followers / 1000000).toFixed(2)}m`;
-    }
-    
-
-    displayData.innerHTML = `
-        <img src="${data.avatar}" alt="${userInput.value}'s Avatar">
-        <h2>Name: ${titleHtml} ${data.name}</h2>
-        <h3>Username: ${data.username}</h3>
-        <p><b>Followers:</b> ${data.followers}</p>
-        <p><b>Country:</b> ${dataCountry.name}</p>
-        <p><b>League:</b> ${data.league}</p>
-        <p><b>Chess.com URL:</b> <a target="_blank" href="${data.url}">${data.url}</a></p>
-        <br>
-        <div class="rapid">
-            <p>Rapid</p>
-            <p>Rating: ${dataStats.chess_rapid.last.rating}</p>
-            <p>Win/Loss/Draw Record: ${dataStats.chess_rapid.record.win}/${dataStats.chess_rapid.record.loss}/${dataStats.chess_rapid.record.draw}</p>
-        </div>
-        <br>
-        ${blitzHtml}
-        ${bulletHtml}
-    `;
 }
 
 const displayPuzzle = async () => {
